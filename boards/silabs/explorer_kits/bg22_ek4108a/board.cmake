8b5896c9f005396f9354ccaf88f4202bfb8d1c65
Ian Morris <ian.d.morris@outlook.com>
2024-07-27 11:26:22 -0700

    update me!!
    Signed-off-by: Ian Morris <ian.d.morris@outlook.com>

---
+++ boards/silabs/explorer_kits/bg22_ek4108a/board.cmake
@@ -0,0 +1,6 @@
+# Copyright (c) 2024 Ian Morris
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(jlink "--device=EFR32BG22C224F512IM40" "--reset-after-load")
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
