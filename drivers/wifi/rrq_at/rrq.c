3cb2346ff1718ae5471caa5e9ea8ef2bcc6f4bde
Ian Morris <ian.d.morris@outlook.com>
2025-02-01 14:33:24 -0800

    update

--- drivers/wifi/rrq_at/rrq.c
+++ drivers/wifi/rrq_at/rrq.c
@@ -11,6 +11,31 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(wifi_rrq_at, CONFIG_WIFI_LOG_LEVEL);
 
+#include <zephyr/drivers/gpio.h>
+
+#include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/conn_mgr/connectivity_wifi_mgmt.h>
+
+#include "rrq_offload.h"
+
+struct rrq_config {
+#if DT_INST_NODE_HAS_PROP(0, reset_gpios)
+	const struct gpio_dt_spec reset;
+#endif
+};
+
+struct rrq_data {
+	struct net_if *net_iface;
+};
+
+static const struct rrq_config rrq_driver_config = {
+#if DT_INST_NODE_HAS_PROP(0, reset_gpios)
+	.reset = GPIO_DT_SPEC_INST_GET(0, reset_gpios),
+#endif
+};
+
+struct rrq_data rrq_driver_data;
+
 static int rrq_mgmt_scan(const struct device *dev,
 			 struct wifi_scan_params *params,
 			 scan_result_cb_t cb)
@@ -48,6 +73,10 @@ static int rrq_mgmt_iface_status(const struct device *dev,
 
 static void rrq_iface_init(struct net_if *iface)
 {
+	rrq_offload_init(iface);
+
+	/* Not currently connected to a network */
+	net_if_dormant_on(iface);
 }
 
 static enum offloaded_net_if_types rrq_offload_get_type(void)
@@ -70,6 +99,19 @@ static const struct net_wifi_mgmt_offload rrq_api = {
 	.wifi_mgmt_api = &rrq_mgmt_ops,
 };
 
+static int rrq_reset(const struct device *dev)
+{
+#if DT_INST_NODE_HAS_PROP(0, reset_gpios)
+	const struct rrq_config *config = dev->config;
+
+	gpio_pin_set_dt(&config->reset, 1);
+	k_sleep(K_MSEC(100));
+	gpio_pin_set_dt(&config->reset, 0);
+#endif
+
+	return 0;
+}
+
 static int rrq_init(const struct device *dev);
 
 /* The network device must be instantiated above the init function in order
@@ -85,5 +127,9 @@ CONNECTIVITY_WIFI_MGMT_BIND(Z_DEVICE_DT_DEV_ID(DT_DRV_INST(0)));
 
 static int rrq_init(const struct device *dev)
 {
-	return 0;
+	int ret = 0;
+
+	ret = rrq_reset(dev);
+
+	return ret;
 }
