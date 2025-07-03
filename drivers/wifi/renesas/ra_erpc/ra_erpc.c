7f34af4bae6182845772cd767ec3e34589d62e33
Ian Morris <ian.morris.vf@renesas.com>
2025-07-03 14:41:29 -0700

    WIP: Added support for iface_status function.

--- drivers/wifi/renesas/ra_erpc/ra_erpc.c
+++ drivers/wifi/renesas/ra_erpc/ra_erpc.c
@@ -21,34 +21,21 @@ LOG_MODULE_REGISTER(wifi_ra_erpc, CONFIG_WIFI_LOG_LEVEL);
 #include <erpc_transport_setup.h>
 #include <erpc_mbf_setup.h>
 #include <erpc_arbitrated_client_setup.h>
+#include <c_wifi_host_to_ra_client.h>
+#include <c_wifi_ra_to_host_server.h>
 
 #include "ra_erpc.h"
 #include "ra_erpc_socket_offload.h"
-#include "c_wifi_host_to_ra_client.h"
-#include "c_wifi_ra_to_host_server.h"
 
-// TODO - make these Kconfig symbols?
-#define SERVER_THREAD_STACK_SIZE 1024
-#define SERVER_THREAD_PRIORITY 5
-
-// TODO - use a better name and make a Kconfig symbol
-#define IP_ASSIGN_TIMEOUT		K_SECONDS(5)
-
-// TODO can all these be static?
-erpc_server_t server;
-void erpc_server_thread(void *arg1, void *arg2, void *arg3);
-void erpc_client_error_handler(erpc_status_t err, uint32_t functionID);
-
-K_THREAD_STACK_DEFINE(erpc_server_stack_area, SERVER_THREAD_STACK_SIZE);
-static struct k_thread erpc_server_thread_data;
+K_THREAD_STACK_DEFINE(ra_erpc_server_thread_stack, 
+			  CONFIG_WIFI_RA_ERPC_SERVER_THREAD_STACK_SIZE);
+static struct k_thread ra_erpc_server_thread_data;
 
 K_KERNEL_STACK_DEFINE(ra_erpc_workq_stack,
 		      CONFIG_WIFI_RA_ERPC_WORKQ_STACK_SIZE);
 
 // TODO can this be static?
 struct ra_erpc_data ra_erpc_driver_data;
-
-static K_SEM_DEFINE(ip_acquired, 0, 1);
 
 static inline enum wifi_security_type drv_to_wifi_mgmt_sec(int drv_security_type)
 {
@@ -94,7 +81,6 @@ static int ra_erpc_mgmt_scan(const struct device *dev,
 	struct wifi_scan_params *params,
 	scan_result_cb_t cb)
 {
-	int err;
 	struct ra_erpc_data *data = dev->data;
 
 	LOG_DBG("ra_erpc_mgmt_scan");
@@ -110,10 +96,9 @@ static int ra_erpc_mgmt_scan(const struct device *dev,
 
 	data->scan_cb = cb;
 	data->scan_max_bss_cnt = params->max_bss_cnt;
+	data->state = WIFI_STATE_SCANNING;
 
-	err = k_work_submit_to_queue(&data->workq, &data->scan_work);
-
-	LOG_DBG("k_work_submit_to_queue - err: %d", err);
+	k_work_submit_to_queue(&data->workq, &data->scan_work);
 
 	return 0;
 }
@@ -172,7 +157,7 @@ static void ra_erpc_mgmt_scan_work(struct k_work *work)
 				k_yield();
 			}
 		} else {
-			// TODO - pass back an error code?
+			// TODO - pass back an enumerated error code?
 			dev->scan_cb(dev->net_iface, -1, NULL);
 		}
 		free(results);
@@ -180,12 +165,12 @@ static void ra_erpc_mgmt_scan_work(struct k_work *work)
 
 	dev->scan_cb(dev->net_iface, 0, NULL);
 	dev->scan_cb = NULL;
+	dev->state = WIFI_STATE_DISCONNECTED;
 }
 
 static int ra_erpc_mgmt_connect(const struct device *dev,
 	struct wifi_connect_req_params *params)
 {
-	int err;
 	struct ra_erpc_data *data = dev->data;
 
 	LOG_DBG("ra_erpc_mgmt_connect");
@@ -204,9 +189,7 @@ static int ra_erpc_mgmt_connect(const struct device *dev,
 		data->drv_nwk_params.xPassword.xWPA.ucLength);
 	data->drv_nwk_params.ucChannel = params->channel;
 
-	err = k_work_submit_to_queue(&data->workq, &data->connect_work);
-
-	LOG_DBG("k_work_submit_to_queue - err: %d", err);
+	k_work_submit_to_queue(&data->workq, &data->connect_work);
 
 	return 0;
 }
@@ -215,7 +198,7 @@ static void ra_erpc_mgmt_connect_work(struct k_work *work)
 {
 	struct ra_erpc_data *dev;
 	WIFIReturnCode_t ret;
-	int status = 0;
+	int status;
 
 	dev = CONTAINER_OF(work, struct ra_erpc_data, connect_work);
 
@@ -230,39 +213,24 @@ static void ra_erpc_mgmt_connect_work(struct k_work *work)
 
 	LOG_DBG("WIFI_ConnectAP: %d", ret);
 
-	if (ret) {
-		// TODO - return a better error code, any cleanup required?
-		status = -1;
-		goto complete;
+	if (ret == eWiFiSuccess) {
+		dev->state = WIFI_STATE_COMPLETED;
+		status = WIFI_STATUS_CONN_SUCCESS;
+		net_if_dormant_off(dev->net_iface);
+	} else {
+		status = WIFI_STATUS_CONN_FAIL;
 	}
 
-	/* Wait for DHCP process on offload device to complete */
-	ret = k_sem_take(&ip_acquired, IP_ASSIGN_TIMEOUT);
-	if (ret < 0) {
-		// TODO - return a better error code, any cleanup required?	
-		status = -1;
-		goto complete;
-	}
-
-	net_if_ipv4_addr_add(dev->net_iface, &dev->ip_addr, NET_ADDR_DHCP, 0);
-	net_if_ipv4_set_gw(dev->net_iface, &dev->gw_addr);
-	net_if_ipv4_set_netmask_by_addr(dev->net_iface, &dev->ip_addr, &dev->netmask);
-	net_if_dormant_off(dev->net_iface);
-
-complete:
 	wifi_mgmt_raise_connect_result_event(dev->net_iface, status);
 }
 
 static int ra_erpc_mgmt_disconnect(const struct device *dev)
 {
-	int err;
 	struct ra_erpc_data *data = dev->data;
 
 	LOG_DBG("ra_erpc_mgmt_disconnect");
 
-	err = k_work_submit_to_queue(&data->workq, &data->disconnect_work);
-
-	LOG_DBG("k_work_submit_to_queue - err: %d", err);
+	k_work_submit_to_queue(&data->workq, &data->disconnect_work);
 
 	return 0;
 }
@@ -282,11 +250,62 @@ static void ra_erpc_mgmt_disconnect_work(struct k_work *work)
 	LOG_DBG("WIFI_Disconnect: %d", ret);
 
 	if (ret != eWiFiSuccess) {
+		// TODO - there an enumerated error code that can be used here?
 		status = -1;
 	}
 
+	dev->state = WIFI_STATE_DISCONNECTED;
+
 	wifi_mgmt_raise_disconnect_result_event(dev->net_iface, status);
 	net_if_dormant_on(dev->net_iface);
+}
+
+int ra_erpc_mgmt_iface_status(const struct device *dev,
+			     struct wifi_iface_status *status)
+{
+	struct ra_erpc_data *data = dev->data;
+
+	LOG_DBG("ra_erpc_mgmt_iface_status");
+	
+	status->state = data->state;
+	// TODO use MIN macro to ensure not too long?
+	status->ssid_len = data->drv_nwk_params.ucSSIDLength;
+	memcpy(status->ssid, data->drv_nwk_params.ucSSID, status->ssid_len);
+
+	// TODO - populate the rest of the status structure
+#if 0
+struct wifi_iface_status {
+	/** BSSID */
+	char bssid[WIFI_MAC_ADDR_LEN];
+	/** Frequency band */
+	enum wifi_frequency_bands band;
+	/** Channel */
+	unsigned int channel;
+	/** Interface mode, see enum wifi_iface_mode */
+	enum wifi_iface_mode iface_mode;
+	/** Link mode, see enum wifi_link_mode */
+	enum wifi_link_mode link_mode;
+	/** WPA3 enterprise type */
+	enum wifi_wpa3_enterprise_type wpa3_ent_type;
+	/** Security type, see enum wifi_security_type */
+	enum wifi_security_type security;
+	/** MFP options, see enum wifi_mfp_options */
+	enum wifi_mfp_options mfp;
+	/** RSSI */
+	int rssi;
+	/** DTIM period */
+	unsigned char dtim_period;
+	/** Beacon interval */
+	unsigned short beacon_interval;
+	/** is TWT capable? */
+	bool twt_capable;
+	/** The current 802.11 PHY TX data rate (in Mbps) */
+	int current_phy_tx_rate;
+};
+
+#endif
+
+	return 0;
 }
 
 static enum offloaded_net_if_types ra_erpc_offload_get_type(void)
@@ -302,10 +321,57 @@ static void ra_erpc_iface_init(struct net_if *iface)
 	net_if_dormant_on(iface);
 }
 
+static void ra_erpc_client_error_handler(erpc_status_t err, uint32_t func_id)
+{
+	if (err > 0) {
+		/* See wifi_interface.hpp for list of function id's */
+		LOG_ERR("eRPC client error - err: %d func_id: %d\n", err, func_id);
+	}
+}
+
+static void ra_erpc_server_thread(void *arg1, void *unused1, void *unused2) 
+{
+	struct ra_erpc_data *data = (struct ra_erpc_data *)arg1;
+
+	ARG_UNUSED(unused1);
+	ARG_UNUSED(unused2);
+
+	while (1) {
+		// TODO should we use erpc_server_run() instead? 
+		erpc_status_t err = erpc_server_poll(data->erpc_server);
+		if (err != kErpcStatus_Success) {
+			LOG_ERR("erpc_server_poll - err: %d\n", err);
+		}
+	}
+}
+
+void ra_erpc_server_event_handler(const ra_erp_server_event_t * event)
+{
+    LOG_DBG("ra_erpc_server_event_handler - event_id: %d", event->event_id);
+
+    switch (event->event_id) {
+        case eNetworkInterfaceIPAssigned:
+        {
+			// TODO - handle address type, get rid of warnings, basically clean this up!
+			net_if_ipv4_addr_add(ra_erpc_driver_data.net_iface,
+				 &event->event_data.xConfig.xIPAddress.ulAddress[0], NET_ADDR_DHCP, 0);
+			net_if_ipv4_set_gw(ra_erpc_driver_data.net_iface, 
+				 &event->event_data.xConfig.xGateway.ulAddress[0]);
+			net_if_ipv4_set_netmask_by_addr(ra_erpc_driver_data.net_iface, 
+				 &event->event_data.xConfig.xIPAddress.ulAddress[0], 
+				 &event->event_data.xConfig.xNetMask.ulAddress[0]);
+            break;
+        }
+        default:
+            break;
+    }
+}
+
 static const struct wifi_mgmt_ops ra_erpc_mgmt_ops = {
 	.scan		   		= ra_erpc_mgmt_scan,
 	.connect	   		= ra_erpc_mgmt_connect,
 	.disconnect	   		= ra_erpc_mgmt_disconnect,
+	.iface_status		= ra_erpc_mgmt_iface_status,
 #ifdef CONFIG_WIFI_RA_ERPC_SOFTAP_SUPPORT
 	.ap_enable     		= NULL,
 	.ap_disable    		= NULL,
@@ -331,14 +397,15 @@ CONNECTIVITY_WIFI_MGMT_BIND(Z_DEVICE_DT_DEV_ID(DT_DRV_INST(0)));
 
 static int ra_erpc_init(const struct device *dev)
 {
-    erpc_transport_t transport;
-    erpc_mbf_t message_buffer_factory;
-    erpc_transport_t arbitrator;
-    erpc_client_t client_manager;
-    erpc_service_t service;
+	erpc_transport_t transport;
+	erpc_mbf_t mbf;
+	erpc_transport_t arbitrator;
+	erpc_client_t client_manager;
+	erpc_service_t service;
 
 	struct ra_erpc_data *data = dev->data;
 
+	// TODO - Fix this!!!!
 	/* Temporary fix to allow RA6W1 to empty UART buffer of incorrect
 	   byte(s) it receives when host MCU is reset. */
 	k_msleep(1000);
@@ -361,88 +428,45 @@ static int ra_erpc_init(const struct device *dev)
 			   NULL);
 	k_thread_name_set(&data->workq.thread, "ra_erpc_workq");
 
-    /* Initialize the eRPC client infrastructure */
-    transport = erpc_transport_zephyr_uart_init((void *)data->bus);
+	/* Initialize the eRPC client infrastructure */
+	transport = erpc_transport_zephyr_uart_init((void *)data->bus);
 	if (transport == NULL) {
 		LOG_ERR("Failed to initialize eRPC transport");
 		return -ENODEV;
 	}
 
-    message_buffer_factory = erpc_mbf_dynamic_init();
-	if (message_buffer_factory == NULL) {
+	mbf = erpc_mbf_dynamic_init();
+	if (mbf == NULL) {
 		LOG_ERR("Failed to initialize eRPC message buffer factory");
 		return -ENODEV;
 	}
 
-    client_manager = erpc_arbitrated_client_init(transport, message_buffer_factory, &arbitrator);
+	client_manager = erpc_arbitrated_client_init(transport, mbf, &arbitrator);
 	if (client_manager == NULL) {
 		LOG_ERR("Failed to initialize eRPC client");
 		return -ENODEV;
 	}
 
-    // Init eRPC client interface
-    erpc_client_set_error_handler(client_manager, erpc_client_error_handler);
+	/* Initialize eRPC client interface */
+	erpc_client_set_error_handler(client_manager, ra_erpc_client_error_handler);
 	initwifi_client(client_manager);
 
-    // Init eRPC server interface
-    server = erpc_server_init(arbitrator, message_buffer_factory);    
-    service = create_wifi_async_service();
-    /* Add custom service implementation to the server */
-    erpc_add_service_to_server(server, service);
+	/* Initialize eRPC server interface */
+	data->erpc_server = erpc_server_init(arbitrator, mbf);
+	service = create_wifi_async_service();
 
-    k_thread_create(&erpc_server_thread_data, 
-                    erpc_server_stack_area, 
-                    SERVER_THREAD_STACK_SIZE,
-                    erpc_server_thread,
-                    NULL, NULL, NULL,  // arguments
-                    SERVER_THREAD_PRIORITY, 0,       // priority and options
-                    K_NO_WAIT);        // start immediately
+	/* Add custom service implementation to the server */
+	erpc_add_service_to_server(data->erpc_server, service);
+
+	k_thread_create(&ra_erpc_server_thread_data,
+					ra_erpc_server_thread_stack,
+					CONFIG_WIFI_RA_ERPC_SERVER_THREAD_STACK_SIZE,
+					ra_erpc_server_thread,
+					data, NULL, NULL,
+					CONFIG_WIFI_RA_ERPC_SERVER_THREAD_PRIORITY, 0,
+					K_NO_WAIT);
 
 	data->net_iface = NET_IF_GET(Z_DEVICE_DT_DEV_ID(DT_DRV_INST(0)), 0);
 
 	return 0;
-}
-
-void erpc_client_error_handler(erpc_status_t err, uint32_t functionID)
-{
-    if(err > 0) {
-        // see wifi_interface.hpp for list of function IDs
-        LOG_ERR("eRPC client error. err=%d, functionID=%d\n", err, functionID);
-    }
-}     
-
-// This thread polls the server interface (e.g. has RA6W1 called `async` function)
-void erpc_server_thread(void *arg1, void *arg2, void *arg3) {
-    while (1) {
-        // TODO should we use erpc_server_run() instead? 
-        erpc_status_t err = erpc_server_poll(server);
-        if (err != kErpcStatus_Success)
-        {
-            LOG_ERR("erpc_server_poll() error=%d\n", err);
-        }
-    }
-}
-
-void ra_erpc_server_event_handler(const ra_erp_server_event_t * event)
-{
-    LOG_DBG("ra_erpc_server_event_handler - event_id: %d", event->event_id);
-
-    switch(event->event_id) {
-        case eNetworkInterfaceIPAssigned:
-        {
-			memcpy(&ra_erpc_driver_data.ip_addr, &event->event_data.data, sizeof(event->event_data.data));
-			memcpy(&ra_erpc_driver_data.ip_addr, &event->event_data.xConfig.xIPAddress.ulAddress[0], sizeof(ra_erpc_driver_data.ip_addr));
-			memcpy(&ra_erpc_driver_data.gw_addr, &event->event_data.xConfig.xGateway.ulAddress[0], sizeof(ra_erpc_driver_data.gw_addr));
-			memcpy(&ra_erpc_driver_data.netmask, &event->event_data.xConfig.xNetMask.ulAddress[0], sizeof(ra_erpc_driver_data.netmask));
-			k_sem_give(&ip_acquired);
-
-            char buf[INET_ADDRSTRLEN ];
-            if (net_addr_ntop(AF_INET, &ra_erpc_driver_data.ip_addr, buf, sizeof(buf))) {
-                LOG_DBG("IPv4 addr = %s\n", buf);
-            }
-            break;
-        }
-        default:
-            break;
-    }
 }
--- drivers/wifi/renesas/ra_erpc/ra_erpc.h
+++ drivers/wifi/renesas/ra_erpc/ra_erpc.h
@@ -8,7 +8,8 @@
 #define ZEPHYR_INCLUDE_DRIVERS_WIFI_RENESAS_RA_ERPC_H_
 
 #include <zephyr/net/wifi_mgmt.h>
-#include "wifi_host_to_ra_common.h"
+#include <wifi_host_to_ra_common.h>
+#include <erpc_server_setup.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -17,12 +18,14 @@ extern "C" {
 struct ra_erpc_data {
 	const struct device *bus;
 	struct net_if *net_iface;
+	enum wifi_iface_state state;
 	scan_result_cb_t scan_cb;
 	uint16_t scan_max_bss_cnt;
 	struct WIFINetworkParams_t drv_nwk_params;
-	struct in_addr ip_addr;
-	struct in_addr gw_addr;
-	struct in_addr netmask;
+	//struct in_addr ip_addr;
+	//struct in_addr gw_addr;
+	//struct in_addr netmask;
+	erpc_server_t erpc_server;
 
 	struct k_work_q workq;
 	struct k_work scan_work;
